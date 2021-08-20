d930cc3612a493889b64eaa919b0ab8a2a3642b4
Thomasraf150 <54179908+Thomasraf150@users.noreply.github.com>
2021-08-20 16:35:57 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./budget.scss";
+import React, { useState } from "react";
 
 function App() {
+  const [state, setState] = useState({ originalValue: "", fifty: "", thirty: "", twenty: "" });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const calculate = (event) => {
+    event.preventDefault();
+    var fiftyPercent = state.originalValue * 0.5;
+    var thirtyPercent = state.originalValue * 0.3;
+    var twentyPercent = state.originalValue * 0.2;
+    setState({
+      originalValue: state.originalValue,
+      fifty: fiftyPercent,
+      thirty: thirtyPercent,
+      twenty: twentyPercent,
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="app-title">Cruz Calculator</div>
+        <div className="input-budget">
+          <label>Please input the amount you want to budget!</label>
+          <input name="originalValue" type="number" onChange={handleChange} />
+        </div>
+
+        <div className="results">
+          <section>
+            <label>50% = </label>
+            <input name="fifth" disabled value={state.fifty} />
+          </section>
+          <section>
+            <label>30% = </label>
+            <input name="thirty" disabled value={state.thirty} />
+          </section>
+          <section>
+            <label>20% = </label>
+            <input name="twenty" disabled value={state.twenty} />
+          </section>
+        </div>
+
+        <div className="calculation-button">
+          <input type="button" value="Calculate!" onClick={calculate} />
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
